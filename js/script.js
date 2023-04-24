44b4c56ddb185e02f75b2d0fbb73eee6d3a16ab5
EverettBernard <124380095+EverettBernard@users.noreply.github.com>
2023-04-24 15:05:58 +0000

    need to do dropdowns

--- js/script.js
+++ js/script.js
@@ -4,48 +4,93 @@
 "use strict";
 
 /**
- * This function calculates what movies the user can watch
+ * This function calculates the users order and how much it will cost
  */
 function calculate () {
   
-  // user age input
-  let userage = parseFloat(document.getElementById('Uage').value);
-  let agestate = ""
-  // If statments for age
- if (userage >=17) {
-
-        agestate = "You can watch any movie."
+  // user order input
+  let psize = parseFloat(document.getElementById('size').value);
+  let dsize = parseFloat(document.getElementById('Dsize').value);
+  let topping = parseFloat(document.getElementById('toppings').value);
+  let pizza = ""
+  let drink = ""
+  let Tnumber= ""
+  // If statments for pizza size
+ if ( psize ==1) {
+   
+   pizza = 10
 
     }
 
-    else if (userage >= 13) {
+  else if ( psize ==2) {
 
-        agestate = "You can watch PG-13 movies."
+    pizza = 15
 
     }  
 
-	else if (userage >=5) {
+	else if ( psize ==3) {
 
-        agestate = "You can watch PG movies."
+    pizza = 20
 
     }
-    else if (userage >0) {
-
-        agestate = "You can not watch any movies alone."
-    }
-    else if (userage <0) {
-
-        agestate = "Invalid age."
-    }
-
-  else {
-
-        agestate = "Invalid age."
+  
+  // If statments for drink size
+ if ( dsize ==1) {
+   
+   drink = 3
 
     }
 
+  else if ( dsize ==2) {
+
+    drink = 5
+
+    }  
+    // If statments for number of toppings
+ if ( topping ==1) {
+   
+   Tnumber = 1
+
+    }
+
+  else if ( topping ==2) {
+
+    Tnumber = 1.50
+
+    }  
+
+	else if ( topping ==3) {
+
+    Tnumber = 2.50
+
+    }
+    
+  else if ( topping ==4) {
+
+    Tnumber = 3
+
+    }
+    
+  else if ( topping ==5) {
+
+    Tnumber = 3.50
+
+    }
+    
+  else if ( topping ==6) {
+
+    Tnumber = 4
+
+    }
+  
+  //subtotal calculation
+  let subtotal = drink+pizza+Tnumber Math
+  let tax = (drink+pizza+Tnumber) * 0.15
+  
   // output what movies user can watch
-  document.getElementById('movie-display').innerHTML = agestate 
+  document.getElementById('movie-display').innerHTML = "Subtotal: $"+subtotal
+  document.getElementById('movie-display2').innerHTML = "Tax: $"+tax
+  
 
 }
  
