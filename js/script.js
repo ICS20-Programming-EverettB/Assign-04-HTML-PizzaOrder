abd2b45e83c0eb60385e9c252e813d56b0081946
EverettBernard <124380095+EverettBernard@users.noreply.github.com>
2023-05-02 15:04:23 +0000

    addded my complex comments

--- js/script.js
+++ js/script.js
@@ -4,20 +4,23 @@
 "use strict";
 
 /**
- * This function calculates the users order and how much the user's order will cost
+ * This function calculates the collects the users order and how much the user's order will cost
  */
 function calculate () {
   
-  // user order input
+  // user order input for drink size, pizza size, toppngs, and side dish
   let psize = (document.getElementById('size').value);
   let dsize = (document.getElementById('Dsize').value);
   let topping = (document.getElementById('toppings').value);
   let dish = (document.getElementById('dish').value);
+
+  //variables used for declairing price and calculating subtotal  
   let pizza = ""
   let drink = ""
   let Tnumber= ""
   let Dselect= ""
-  // If statments for pizza size
+  
+  // If statments for pizza size and how much each size costs, adding cost to subtotal
  if ( psize == "small") {
    
    pizza = 10
@@ -42,7 +45,7 @@ function calculate () {
 
     }
   
-  // If statments for drink size
+  // If statments for drink size and price per size, adding cost to subtotal
  if ( dsize =="small1") {
    
    drink = 3
@@ -60,7 +63,7 @@ function calculate () {
    drink = 0
 
     }
-    // If statments for number of toppings
+  // If statments for number of toppings and price per topping, adding cost to subtotal
  if ( topping =="1t") {
    
    Tnumber = 1
@@ -101,7 +104,7 @@ function calculate () {
    Tnumber = 0
 
     }
-  // If statments for dish
+  // If statments for dish, adding cost to subtotal
  if ( dish == "caesar") {
    
    Dselect = 7
@@ -125,12 +128,12 @@ function calculate () {
 
     }
   
-  //subtotal tax and total calculation
+  //subtotal tax and total calculation by adding all the prices to get subtotal, multiplying by .15 to get tax then adding tax and subtotal to get total.
   let subtotal = drink+pizza+Tnumber+Dselect
   let tax = (drink+pizza+Tnumber) * 0.15
   let total = tax + subtotal
   
-  // output subtotal, tax and total
+  // output subtotal, tax and total for user to see with $ symbol and text explaining each 
   document.getElementById('sub-display').innerHTML = "Subtotal: $"+subtotal.toFixed(2) 
   document.getElementById('tax-display').innerHTML = "Tax: $"+tax.toFixed(2)
   document.getElementById('total-display').innerHTML = "Total: $"+total.toFixed(2)
